a28692b1654766249ed111b1913a47f664bc3ef5
Matt Larraz <mlarraz@users.noreply.github.com>
2025-04-17 14:09:55 -0400

    Rails 7.2

--- lib/odbc_adapter.rb
+++ lib/odbc_adapter.rb
@@ -1,2 +1,6 @@
 # Requiring with this pattern to mirror ActiveRecord
 require "active_record/connection_adapters/odbc_adapter"
+
+if ActiveRecord::ConnectionAdapters.respond_to?(:register)
+  ActiveRecord::ConnectionAdapters.register("odbc", "ActiveRecord::ConnectionAdapters::ODBCAdapter", "active_record/connection_adapters/odbc_adapter")
+end
--- lib/odbc_adapter/quoting.rb
+++ lib/odbc_adapter/quoting.rb
@@ -1,26 +1,29 @@
 module ODBCAdapter
   module Quoting
+    extend ActiveSupport::Concern
+    module ClassMethods
+      # Returns a quoted form of the column name.
+      def quote_column_name(name)
+        name = name.to_s
+        quote_char = database_metadata.identifier_quote_char.to_s.strip
+
+        return name if quote_char.empty?
+
+        quote_char = quote_char[0]
+
+        # Avoid quoting any already quoted name
+        return name if name[0] == quote_char && name[-1] == quote_char
+
+        # If upcase identifiers, only quote mixed case names.
+        return name if database_metadata.upcase_identifiers? && name !~ /([A-Z]+[a-z])|([a-z]+[A-Z])/
+
+        "#{quote_char.chr}#{name}#{quote_char.chr}"
+      end
+    end
+
     # Quotes a string, escaping any ' (single quote) characters.
     def quote_string(string)
       string.gsub("'", "''")
-    end
-
-    # Returns a quoted form of the column name.
-    def quote_column_name(name)
-      name = name.to_s
-      quote_char = database_metadata.identifier_quote_char.to_s.strip
-
-      return name if quote_char.empty?
-
-      quote_char = quote_char[0]
-
-      # Avoid quoting any already quoted name
-      return name if name[0] == quote_char && name[-1] == quote_char
-
-      # If upcase identifiers, only quote mixed case names.
-      return name if database_metadata.upcase_identifiers? && name !~ /([A-Z]+[a-z])|([a-z]+[A-Z])/
-
-      "#{quote_char.chr}#{name}#{quote_char.chr}"
     end
 
     # Ideally, we'd return an ODBC date or timestamp literal escape
